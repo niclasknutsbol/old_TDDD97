7c14beaef42228c2f0b5125aebb1e0e292f435fd
AntyGit <blixten93@hotmail.com>
2015-01-27 17:46:22 +0100

    Added validation for occupied email adresses

--- client.js
+++ client.js
@@ -1,66 +1,81 @@
-
 var current_view = "welcomeview";
-var min_passw_length = 5;
+var min_passw_length =3;
 
-displayView = function(view) {
-	document.getElementById("content").innerHTML = document.getElementById(view).innerHTML;
+displayView = function(view) 
+{
+   document.getElementById("content").innerHTML = document.getElementById(view).innerHTML;
 };
 
-window.onload = function() {
-	displayView(current_view);
-	document.getElementById("sign-in").onsubmit = function() 
-	{	
-		if(validateSignIn() == true)
-		{
-			current_view = "profileview";
-			displayView(current_view);
+window.onload = function()
+{
+   displayView(current_view);
 
-		}
 
-		return false;
+   //SIGN-IN
+   document.getElementById("sign-in").onsubmit = function() 
+   {	
+      if(validateSignIn() == true)
+      {
+         current_view = "profileview";
+         displayView(current_view);
+      }
+      return false;
+   };
+   /*
+      //TODO SIGN-OUT
+      document.getElementById("logout").onclick
+      {
+         current_view = "welcomeview";
+         displayView( current_view );
+      }
+   */
 
-	};
+   //SIGN-UP
+   document.getElementById("sign-up").onsubmit = function() 
+   {
+      var data_object = 
+      {
+         email: document.getElementById("email2").value,
+         password: document.getElementById("password2").value,
+         firstname: document.getElementById("firstname").value,
+         familyname: document.getElementById("familyname").value, 
+         gender: document.getElementById("gend").value,
+         city: document.getElementById("city").value,
+         country: document.getElementById("country").value
+      };
 
-	document.getElementById("sign-up").onsubmit = function() 
-	{	
-		if(validateSignUp() == true)
-		{
-			var data_object = 
-			{
-				"email": document.getElementById("email").value,
-				"password": document.getElementById("password2").value,
-				"firstname": document.getElementById("firstname").value,
-				"familyname": document.getElementById("familyname").value,
-				"gender": document.getElementById("gend").value,
-				"city": document.getElementById("city").value,
-				"country": document.getElementById("country").value
-			};
+      var response = serverstub.signUp(data_object);
+      if( response.success === false )
+      {
+         var input = document.getElementById('email2');
+         input.setCustomValidity( response.message ); //TODO  
+      } 
+      return false;	
+   };
 
-			var response = serverstub.signUp(data_object);
-			alert(response.message);
-		}
-	
-		return false;
 
-	};
+
 };
 
-validateSignUp = function() {
-	//concole.log("Sign up validated");
-	if (document.getElementById("password2").value === document.getElementById("repeat_psw").value 
-		&& document.getElementById("password2").value.length >= min_passw_length)
- 		return true;
- 	else
- 	{
- 		document.getElementById("password2").focus();
- 		return false;
-	}
+/*
+validateSignUp = function()
+{
+   //concole.log("Sign up validated");
+
+   //We also need to check if the email has been registrated before!
+   if ( 
+ document.getElementById("password2").value.length >= min_passw_length)  //password > minimum lenth
+      {
+         return true;
+      } 
+      else
+      {
+         return false;
+      }
  };
+*/
 
-
-validateSignIn = function() {
-
-	return document.getElementById("password1").value.length >= min_passw_length;
+validateSignIn = function() 
+{
+   
 };
-
-
