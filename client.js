fcb441cae1f0c291e96b2aa05317e7ccf070b4c3
AntyGit <blixten93@hotmail.com>
2015-02-04 12:16:22 +0100

    Finished step 7 and the account tab. Started with step 8 and the home tab

--- client.js
+++ client.js
@@ -12,6 +12,7 @@ signIn = function()
 {
    current_view = "profileview";
    displayView(current_view);
+   window.location.href = "#home_panel";
    init_profile_functions();
 };
 
@@ -20,6 +21,7 @@ SignOut = function()
    localStorage.removeItem( "token" );
    current_view = "welcomeview";
    displayView( current_view );
+   window.location.href = "";
    init_welcome_functions();
 }
 
@@ -111,10 +113,10 @@ init_profile_functions = function()
          ctr_new_password.setCustomValidity("");  
       }
       console.log("#2");
-      var old_password = document.getElementById("password3").value;
+      var old_password = document.getElementById("password3");
       var temp_token = localStorage.getItem( "token" ); 
 
-      var response = serverstub.changePassword( temp_token, old_password, new_password );
+      var response = serverstub.changePassword( temp_token, old_password.value, new_password );
       console.log( response.success );
       if( response.success === false )
       {
