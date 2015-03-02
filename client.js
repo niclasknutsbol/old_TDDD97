b484c39949bccc760d8148662a8c036d4338cf77
Niclas <niclas.jonsson1992@gmail.com>
2015-03-02 12:18:36 +0100

    latest version

--- client.js
+++
@@ -1,8 +0,0 @@
-displayView = function(){
-// the code required to display a view
-};
-window.onload = function(){
-//code that is executed as the page is loaded.
-//You shall put your own custom code here.
-window.alert("Hello TDDD97!");
-};
